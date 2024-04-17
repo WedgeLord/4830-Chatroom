8f260f707a030d17674fed398a6c143ec8473bb9
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-04-17 17:56:10 -0500

    Initialize variables for index.js

--- chatroom/src/index.js
+++ chatroom/src/index.js
@@ -1,1 +1,5 @@
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3000;
