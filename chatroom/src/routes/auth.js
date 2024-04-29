b7ebb73f7d756febf1cdd5fb51a2b411b9eade0b
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-04-28 20:51:32 -0500

    Implemented router post for login, error checking

--- chatroom/src/routes/auth.js
+++ chatroom/src/routes/auth.js
@@ -4,3 +4,22 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      res.status(500).send(err);
+    } else if (!user) {
+      res.status(404).send('User not found');
+    } else {
+      if (user.password !== password) {
+        res.status(401).send('Incorrect password');
+      } else {
+        const token = jwt.sign({ username: user.username }, config.secret);
+        res.json({ token });
+      }
+    }
+  });
+});
+
+module.exports = router;
