cbdfe732f73072ec0a84e9e8cc8ceca7b7efacf9
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-04-28 20:50:37 -0500

    Initialized express, router, User, jwt, and config

--- chatroom/src/routes/auth.js
+++ chatroom/src/routes/auth.js
@@ -1,1 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
 
