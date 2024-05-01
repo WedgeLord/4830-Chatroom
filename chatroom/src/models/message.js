d8f96f2261f0b949ff132659be7c1bbac15791d8
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-05-01 14:41:14 -0500

    Commented out timestamps, TBD

--- chatroom/src/models/message.js
+++ chatroom/src/models/message.js
@@ -3,7 +3,7 @@ const messageSchema = new mongoose.Schema({
     
   content: { type: String, required: true },
     sender: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now }
+    // timestamp: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Message', messageSchema);
