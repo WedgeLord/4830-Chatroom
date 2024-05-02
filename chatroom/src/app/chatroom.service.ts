da74e518ee4fc83d0a88ff0e82c585976c994151
Caleb Foster <calebjfoster20@gmail.com>
2024-05-02 15:21:27 -0500

    made some updates

--- chatroom/src/app/chatroom.service.ts
+++ chatroom/src/app/chatroom.service.ts
@@ -4,10 +4,24 @@ import { HttpClient } from '@angular/common/http';
 import { Post } from './chatroom.models'; // interface model
 import { Subject } from 'rxjs';
 
+@Injectable ({
+  providedIn: 'root'
+})
 export class ChatroomService {
+  username: String = "";
   private posts: Post[] = []; // our array of posts
 
   private postsUpdate = new Subject<Post[]>() // subject to track posts
 
   constructor(private http: HttpClient) {}
+
+  sendMessage(targetUser: String, chat: Post) {
+    //this.http.post<{message: Post}>("localhost:3000/user/" + target, chat).subscribe( () => {
+    this.http.post<{target: String, message: Post}>("http://localhost:3000/", {target: targetUser, message: chat}).subscribe( (res) => {
+      // this code triggers when post-request successful
+      console.log(res);
+      // add code for updating chat
+    });
+  }
+
 }
