955118fb7a915c97e16cd6e1b07ef31f98c7bcfd
cmeyer0121 <149642153+cmeyer0121@users.noreply.github.com>
2024-05-05 14:42:33 -0500

    App.component header/footer

--- chatroom/src/app/app.component.ts
+++ chatroom/src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { HeaderComponentComponent } from "./header.component/header.component.component";
+import { FooterComponentComponent } from "./footer.component/footer.component.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    RouterLink, 
-    RouterLinkActive
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [
+        RouterOutlet,
+        RouterLink,
+        RouterLinkActive,
+        HeaderComponentComponent,
+        FooterComponentComponent
+    ]
 })
 export class AppComponent {
   title = 'chatroom';
