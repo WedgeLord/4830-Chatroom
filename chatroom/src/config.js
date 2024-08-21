5873d751414a2cf7a3e4fea162aa306d7daf64c6
Caleb Foster <81270864+WedgeLord@users.noreply.github.com>
2024-08-21 11:19:54 -0500

    Update config.js

--- chatroom/src/config.js
+++ chatroom/src/config.js
@@ -1,6 +1,4 @@
-/*DON'T FORGET TO CHANGE DB AND SECRET TO ACTUAL VALUES*/
-
 module.exports = {
-  database: 'mongodb://app:Webdev2024@100.27.197.146:27017/chatroom',
-  secret: 'secret_key'
+  database: 'insert mongo database URI here',
+  secret: 'insert secret_key here'
 };
